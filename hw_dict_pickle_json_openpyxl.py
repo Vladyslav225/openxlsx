5cdd2a47a5e7ddd1bf49e8aaf43d8d89d6e290ca
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-09-05 23:55:34 +0300

    .

---
+++ hw_dict_pickle_json_openpyxl.py
@@ -0,0 +1,55 @@
+from openpyxl import Workbook
+
+
+class ListDict_txt_xsl:
+
+     def __init__(self, list_):
+
+         self.list = list_
+         self.dict_ = None
+
+     def convert_list_to_dict(self):
+
+          _dict = {}
+
+          for obj in self.list:
+               _dict[obj] = obj
+
+          self.dict_ = _dict
+
+          return _dict
+
+     def save_in_file(self):
+          
+          if self.dict_:
+               with open('converted_dict.txt', 'w') as file_dict:
+                    file_dict.write(str(self.dict_))
+                    file_dict.close
+
+     def read_from_file(self):
+          
+          with open('converted_dict.txt', 'r') as file:
+               v = file.read()
+               file.close
+
+               return v
+
+     def save_in_xlsx(self):
+
+          wb = Workbook()
+          sheet = wb.active
+          sheet['A1'] = str(self.dict_)
+          wb.save('dict.xlsx')
+          wb.close()
+
+object_list = ListDict_txt_xsl(list_ = [1, 2, 3, 4, 5])
+
+convert = object_list.convert_list_to_dict()
+
+save_file_txt = object_list.save_in_file()
+
+read_file_txt = object_list.read_from_file()
+print(read_file_txt)
+
+save_file_xlsx = object_list.save_in_xlsx()
+
