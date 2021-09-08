955de4e2548f542d1ba6022a277b76dedd64e8c3
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-09-08 23:38:24 +0300

    .

--- hw_dict_pickle_json_openpyxl.py
+++ hw_dict_pickle_json_openpyxl.py
@@ -21,19 +21,17 @@ class ListDict_txt_xlsx:
           return _dict
 
      def save_in_file(self):
-          
-          if self.dict_:
-               with open('converted_dict.txt', 'w') as file_dict:
-                    file_dict.write(str(self.dict_))
-                    file_dict.close
+
+          with open('converted_dict.txt', 'w') as file_dict:
+               file_dict.write(str(self.dict_))
+               file_dict.close()
 
      def read_from_file(self):
           
           with open('converted_dict.txt', 'r') as file:
-               v = file.read()
-               file.close
-
-               return v
+               return eval(file.read())
+               
+               
 
      def save_in_xlsx(self, key, value):
 
@@ -51,17 +49,16 @@ class ListDict_txt_xlsx:
 
      def read_in_xlsx(self):
 
-          dict_res = {}
+          _dict = {}
 
           wb = load_workbook('dict.xlsx')
-          wb.active
-          wb.active
+          sheet = wb.active
+          
+          for key, value in sheet.iter_rows(min_row=2, max_col=2, max_row=6, values_only=True):
+              _dict[key] = key
+              _dict[value] = value              
 
-          for key, value in (self.dict_.items()):
-               dict_res[key] = key
-               dict_res[value] = value
-
-          return dict_res
+          return _dict
           
 
 object_list = ListDict_txt_xlsx(list_ = [1, 2, 3, 4, 5])
@@ -71,9 +68,9 @@ convert = object_list.convert_list_to_dict()
 save_file_txt = object_list.save_in_file()
 
 read_file_txt = object_list.read_from_file()
-print(f'Reading a dictionary from a text file{read_file_txt}')
+print(f'Reading a dictionary from a text file {read_file_txt}')
 
 save_file_xlsx = object_list.save_in_xlsx('key', 'value')
 
 read_file_xlsx = object_list.read_in_xlsx()
-print(f'Reading a dictionary from Exel file{read_file_xlsx}')
+print(f'Reading a dictionary from Exel file {read_file_xlsx}')
